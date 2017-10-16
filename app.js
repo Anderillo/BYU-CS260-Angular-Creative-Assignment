f3db32f902c9c2aa749184bc5585870cd01002b7
Anderillo <anderillohale@gmail.com>
2017-10-16 14:38:08 -0600

    Adjusted test pass display stuff.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 angular.module('javascript', [])
   .controller('mainCtrl', mainCtrl)
+  .filter('html',function($sce){ return function(input) { return $sce.trustAsHtml(input); } })
   .directive('test', testDirective);
   
 function createHeaderVars(args) {
@@ -11,49 +12,81 @@ function createHeaderVars(args) {
   return header;
 }
 
-function mainCtrl($scope) {
+function mainCtrl($scope, $sce) {
   $scope.moreQuestions = true;
   $scope.instructions = 'Instructions';
+  $scope.xMark = ['red', '&#x2717;'];
+  $scope.checkMark = ['green', '&#x2713;']
   $scope.problems = [{
     question:'Write a function that returns the value of a1 and a2 added together.',
     header: 'function addition(var a1, var a2) {',
     footer: '}',
     tests: [{
-        passed: 'red',
+        passed: $scope.xMark,
         argument: [1, 2],
         answer: 3
       },
       {
-        passed: 'red',
+        passed: $scope.xMark,
         argument: [2, 3],
         answer: 5
-    }]
+      },
+      {
+        passed: $scope.xMark,
+        argument: [10, 1000],
+        answer: 1010
+      },
+      {
+        passed: $scope.xMark,
+        argument: [14, 27],
+        answer: 41
+      },
+      {
+        passed: $scope.xMark,
+        argument: [42, 423452345],
+        answer: 423452387
+      }]
   },
   {
     question:'Write a function that returns the value of a1 and a2 multiplied together.',
     header: 'function multiplication(var a1, var a2) {',
     footer: '}',
     tests: [{
-        passed: 'red',
+        passed: $scope.xMark,
         argument: [1, 2],
         answer: 2
       }, 
       {
-        passed: 'red',
+        passed: $scope.xMark,
         argument: [2, 3],
         answer: 6
-    }]
+      },
+      {
+        passed: $scope.xMark,
+        argument: [10, 1000],
+        answer: 10000
+      },
+      {
+        passed: $scope.xMark,
+        argument: [14, 27],
+        answer: 378
+      },
+      {
+        passed: $scope.xMark,
+        argument: [42, 423452345],
+        answer: 17784998490
+      }]
   }];
   $scope.currentIndex = 0;
 
   $scope.runCode = function() {
     var passedAllTests = true;
     for (var i = 0; i < $scope.problems[$scope.currentIndex].tests.length; i++) {
-      $scope.problems[$scope.currentIndex].tests[i].passed = 'red';
+      $scope.problems[$scope.currentIndex].tests[i].passed = $scope.xMark;
       var code = createHeaderVars($scope.problems[$scope.currentIndex].tests[i].argument) + $scope.javascriptForm.javascript;
       var userAnswer = eval('(function() {' + code + '}())');
       if (userAnswer !== $scope.problems[$scope.currentIndex].tests[i].answer) passedAllTests = false;
-      else $scope.problems[$scope.currentIndex].tests[i].passed = 'green';
+      else $scope.problems[$scope.currentIndex].tests[i].passed = $scope.checkMark;
     }
 
     if (passedAllTests) {
@@ -75,9 +108,7 @@ function testDirective() {
     restrict: 'E',
     replace: 'true',
     template: (
-      '<div class="test">' +
-        '<h4 style="color: {{test.passed}}">Status</h4>' +
-      '</div>'
+      '<div class="test" style="color: {{test.passed[0]}};" ng-bind-html="test.passed[1]|html"></div>'
     )
   };
 }
